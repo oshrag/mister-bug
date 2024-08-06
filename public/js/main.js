c2a35811a188217f5b0f27d21e339e0c702fe552
oshrag <oshra.greenblat@gmail.com>
2024-08-06 22:38:10 +0300

    first commit 1

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-
-
-async function onLoadBugs() {
-    const data = await fetch('/api/bug')
-    const bugs = await data.json()
-    const elPre = document.querySelector('pre')
-    elPre.innerText = JSON.stringify(bugs, null, 4)
-}
